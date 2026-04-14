426d15a7ec5a0ce6e14db28e5572614b255d14d0
nadia <nadiaabdelmoatyabdallah@gmail.com>
2026-04-15 00:40:59 +0200

    feat: implement MongoDB connection logic using mongoose

--- src/DB/db.connection.js
+++ src/DB/db.connection.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 import { DB_URI } from "../../config/config.service.js";
 
-const checkDBConnection=async()=>{
+const checkDBConnection = async () => {
     try {
-        await mongoose.connect(DB_URI);
-        console.log(`connected to db successfully!`);
+        await mongoose.connect(DB_URI, {
+            bufferCommands: false,
+            serverSelectionTimeoutMS: 5000,
+            maxPoolSize: 10,
+        });
+        console.log(`connected to db ${DB_URI}`);
     } catch (error) {
-        console.log("can't connect to db", error);
+        console.log("can't connecte to db", error);
     }
 }
 export default checkDBConnection
