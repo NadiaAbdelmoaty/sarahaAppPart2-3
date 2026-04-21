3c86cc6cf8316ef9a553815d49b198ac4f732e2d
nadia <nadiaabdelmoatyabdallah@gmail.com>
2026-04-21 15:39:07 +0200

    feat: add Joi validation rules for email, password, and MongoDB ObjectId

--- src/common/utlits/generalRules.js
+++ src/common/utlits/generalRules.js
@@ -10,18 +10,18 @@ export const generaRules = {
     )
     .messages({ "any.required": "pass must be here" }),
   cPassword: joi.string().valid(joi.ref("password")),
-  file: joi
-    .object({
-      fieldname: joi.string().required(),
-      originalname: joi.string().required(),
-      encoding: joi.string().required(),
-      mimetype: joi.string().required(),
-      destination: joi.string().required(),
-      filename: joi.string().required(),
-      path: joi.string().required(),
-      size: joi.number().required(),
-    })
-    .messages({ "any.required": "file is required hi" }),
+  // file: joi
+  //   .object({
+  //     fieldname: joi.string().required(),
+  //     originalname: joi.string().required(),
+  //     encoding: joi.string().required(),
+  //     mimetype: joi.string().required(),
+  //     destination: joi.string().required(),
+  //     filename: joi.string().required(),
+  //     path: joi.string().required(),
+  //     size: joi.number().required(),
+  //   })
+  //   .messages({ "any.required": "file is required hi" }),
   id: joi.string().custom((value, helper) => {
     const isvalid = mongoose.Types.ObjectId.isValid(value);
     return isvalid ? value : helper.message("invalid id");
